4ada1448c32b1c964d7ef763841111a597aafa73
Van Duong <vduong@ata-llc.com>
2019-08-20 16:11:42 -0400

    Updated reference to script

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -56,23 +56,26 @@ sed -i -e "s/^#pty=False/pty=False/" /etc/ansible/ansible.cfg
 sed -i -e "s/^#stdout_callback = skippy/stdout_callback = skippy/" /etc/ansible/ansible.cfg
 
 # Cloning Ansible playbook repository
-if [ -d /home/$SUDOUSER/openshift-ansible-ata ]
+if [ -d /home/$SUDOUSER/openshift-origin ]
 then
   echo " - Cleaning old playbooks"
-  rm -rf /home/${SUDOUSER}/openshift-ansible-ata
+  rm -rf /home/${SUDOUSER}/openshift-origin
 else
   echo " - Retrieving playbooks"
 fi
-runuser -l $SUDOUSER -c "git clone --recurse-submodules -b existing-network-3.9 https://github.com/ATALLC/openshift-origin.git /home/$SUDOUSER/openshift-ansible-ata"
-chmod -R 777 /home/$SUDOUSER/openshift-ansible-ata
-if [ -d /home/${SUDOUSER}/openshift-container-platform-playbooks ]
+
+# Cloning Ansible playbook repository
+((cd /home/$SUDOUSER && git clone --recurse-submodules -b existing-network-3.9 https://github.com/ATALLC/openshift-origin.git) || (cd openshift-origin && git pull))
+if [ -d /home/${SUDOUSER}/openshift-origin ]
 then
   echo " - Retrieved playbooks successfully"
-  mv /home/${SUDOUSER}/openshift-ansible-ata/openshift-container-platform-playbooks /home/${SUDOUSER}/openshift-container-platform-playbooks
 else
   echo " - Retrieval of playbooks failed"
   exit 99
 fi
+  
+mv /home/${SUDOUSER}/openshift-origin/openshift-container-platform-playbooks /home/${SUDOUSER}/openshift-container-platform-playbooks
+
 # Create playbook to update ansible.cfg file
 
 cat > updateansiblecfg.yaml <<EOF
@@ -253,7 +256,8 @@ $nodegroup
 EOF
 
 echo $(date) " - Cloning openshift-ansible repo for use in installation"
-mv /home/$SUDOUSER/openshift-ansible-ata/openshift-ansible /home/$SUDOUSER/openshift-ansible
+mv /home/$SUDOUSER/openshift-origin/openshift-ansible /home/$SUDOUSER/openshift-ansible
+chmod -R 777 /home/$SUDOUSER/openshift-ansible
 
 # Run a loop playbook to ensure DNS Hostname resolution is working prior to continuing with script
 echo $(date) " - Running DNS Hostname resolution check"
