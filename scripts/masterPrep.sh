c1ee898276ee009b341a9ea217f79d51a136f904
Van Duong <vduong@ata-llc.com>
2019-08-21 17:11:54 -0400

    pull rpms from github for now

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -33,6 +33,25 @@ then
     echo $(date) " - Installing Ansible"
     ### ADD THE COMMANDS TO COPY THE RPMS ###
     #COPY the RPMS to /tmp/ansible-rpms/
+    wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/ansible-2.6.2-1.el7.ans.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/libyaml-0.1.4-11.el7_0.x86_64.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-babel-0.9.6-8.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-backports-1.0-8.el7.x86_64.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-cffi-1.6.0-5.el7.x86_64.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-enum34-1.0.4-1.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-idna-2.4-1.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-ipaddress-1.0.16-2.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-jinja2-2.7.2-3.el7_6.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-markupsafe-0.11-10.el7.x86_64.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-paramiko-2.1.1-9.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-ply-3.4-11.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-pycparser-2.14-1.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-setuptools-0.9.8-7.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python-six-1.9.0-2.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python2-cryptography-1.7.2-2.el7.x86_64.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/python2-pyasn1-0.1.9-7.el7.noarch.rpm
+	  wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/PyYAML-3.10-11.el7.x86_64.rpm
+    wget -P /tmp/ansible-rpms/ https://raw.githubusercontent.com/ATALLC/openshift-origin/existing-network-3.9/ansible-rpms/sshpass-1.06-2.el7.x86_64.rpm
 	  yum -y install /tmp/ansible-rpms/*.rpm
 fi
 
